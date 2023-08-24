ac15d79858b3525e2efcc6bf58576bfb1ca32144
dannyEnagu <danielEnagu@gmail.com>
2023-08-24 14:04:23 +0000

    Add comments

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -20,13 +20,15 @@ def makeChange(coins, total):
     number_of_coins = 0
 
     for coin in coins:
+        # Count number of time coin is gotten from total
         count = total // coin
+        # Get reminder after counting coin in total
         remainder = total % coin
 
         if remainder == 0:
             number_of_coins += count
             return number_of_coins
         else:
-            total = remainder
+            total = remainder  # Update total
             number_of_coins += count
     return -1
